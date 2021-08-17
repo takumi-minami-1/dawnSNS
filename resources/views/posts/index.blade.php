d395dee4285afc84438bc920ed2bd89c0277d132
takumi-minami-1 <reclamationttt@icloud.com>
2021-08-17 17:25:03 +0900

    4.x.1 モーダルの設置（質問0817）

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -81,8 +81,10 @@
           </div>
         </div>
 
-        <input src='images/trash_h.png' type="image" class="delete-menu-icon" href="/delete"></input>
+        <!-- <input src='images/trash_h.png' type="image" class="delete-menu-icon" href="/delete"></input> -->
+        <!-- <a class="delete-menu-icon" src='images/trash_h.png' type="image" href="/delete" onclick="return confirm('このつぶやきを削除します。よろしいでしょうか？')"></a> -->
 
+        <a href="/delete"><img src="images/trash_h.png" alt="削除" onclick="return confirm('このつぶやきを削除します。よろしいでしょうか？')"></a>
 
 
 
