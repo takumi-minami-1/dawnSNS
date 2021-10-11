0c74c879f430525093767c66c5abedbc07697541
takumi-minami-1 <reclamationttt@icloud.com>
2021-10-11 14:49:44 +0900

    バリデーションのエラー文の表示（修正1008）

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,6 +22,9 @@
         {{ Form::label('UserName',null,['class' => 'Label']) }}
         {{ Form::text('username',null,['class' => 'formText']) }}
       </div>
+      @error('username')
+      <div class="alert alert-danger">{{ $message }}</div>
+      @enderror
 
       <div class="form-item">
         {{ Form::label('MailAddress',null,['class' => 'Label']) }}
